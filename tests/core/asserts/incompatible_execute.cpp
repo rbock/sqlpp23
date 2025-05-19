67c50144c107c5ed7128b785f82538d4527d38b6
Roland Bock <rbock@eudoxos.de>
2025-05-19 07:25:41 +0200

    Fix test failure

--- tests/core/asserts/incompatible_execute.cpp
+++ tests/core/asserts/incompatible_execute.cpp
@@ -27,6 +27,7 @@
 #include <sqlpp23/mock_db/database/connection.h>
 #include <sqlpp23/sqlpp23.h>
 #include <sqlpp23/tests/core/incompatible.h>
+#include <sqlpp23/tests/core/make_test_connection.h>
 #include <sqlpp23/tests/core/tables.h>
 
 int main() {
