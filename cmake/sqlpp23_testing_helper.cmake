5913743450a38424e84d11a244b98b7935e45c3e
MeanSquaredError <35379301+MeanSquaredError@users.noreply.github.com>
2026-06-27 17:00:39 +0300

    CMake: Move the common code that creates test targets into add_testing_target()

---
+++ cmake/sqlpp23_testing_helper.cmake
@@ -0,0 +1,65 @@
+# Copyright (c) 2026, Vesselin Atanasov
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this
+#    list of conditions and the following disclaimer in the documentation and/or
+#    other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+function(add_testing_target)
+    set(options)
+    set(oneValueArgs NAME)
+    set(multiValueArgs DEFINES MOD_DEPS)
+    cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    set(BASE_DIR "${PROJECT_SOURCE_DIR}/tests/${ARG_NAME}")
+    if(BUILD_WITH_MODULES)
+        # We are building with modules, so the _testing target created by this function MUST be a regular
+        # (non-INTERFACE) library. Otherwise, the object files owned by the OBJECT library dependencies
+        # in MOD_DEPS won't be linked, and we are going to get missing symbols during the linking (namely
+        # the static module initializer function won't be found).
+        #
+        # For details on how OBJECT libraries are linked see
+        # https://cmake.org/cmake/help/latest/manual/cmake-buildsystem.7.html#object-libraries
+        # https://cmake.org/cmake/help/latest/command/target_link_libraries.html#linking-object-libraries
+        set(LIB_TYPE)
+        set(LIB_PROP_SCOPE PUBLIC)
+    else()
+        # We are building without modules and the _testing target created by this function won't have any
+        # compiled files, and CMake will not let us build a empty regular (non-INTERFACE) library. So in
+        # this case we have to use an INTERFACE library.
+        set(LIB_TYPE INTERFACE)
+        set(LIB_PROP_SCOPE INTERFACE)
+    endif()
+    set(TARGET_NAME "sqlpp23_${ARG_NAME}_testing")
+    add_library(${TARGET_NAME} ${LIB_TYPE})
+    if(ARG_DEFINES)
+        target_compile_definitions(${TARGET_NAME} ${LIB_PROP_SCOPE} ${ARG_DEFINES})
+    endif()
+    target_include_directories(${TARGET_NAME} ${LIB_PROP_SCOPE} "${BASE_DIR}")
+    target_link_libraries(${TARGET_NAME} ${LIB_PROP_SCOPE} sqlpp23_testing)
+    if(BUILD_WITH_MODULES)
+        target_sources(
+            ${TARGET_NAME}
+            PUBLIC
+                FILE_SET CXX_MODULES FILES "${BASE_DIR}/modules/sqlpp23.test.${ARG_NAME}.tables.cppm"
+        )
+        target_link_libraries(${TARGET_NAME} PUBLIC sqlpp23::core_module ${ARG_MOD_DEPS})
+    endif()
+endfunction()
